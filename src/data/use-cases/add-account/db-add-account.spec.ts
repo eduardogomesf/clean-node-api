d83c5bd197128d889051c598e999622b5e9dc229
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-05-29 21:14:34 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -50,7 +50,7 @@ describe('DbAddAccount Use case', () => {
 
     const accountData = {
       name: 'valid_name',
-      email: 'valid_email',
+      email: 'valid_email@mail.com',
       password: 'valid_password'
     }
 
@@ -66,7 +66,7 @@ describe('DbAddAccount Use case', () => {
 
     const accountData = {
       name: 'valid_name',
-      email: 'valid_email',
+      email: 'valid_email@mail.com',
       password: 'valid_password'
     }
 
@@ -82,7 +82,7 @@ describe('DbAddAccount Use case', () => {
 
     const accountData = {
       name: 'valid_name',
-      email: 'valid_email',
+      email: 'valid_email@mail.com',
       password: 'valid_password'
     }
 
@@ -90,7 +90,7 @@ describe('DbAddAccount Use case', () => {
 
     expect(addSpy).toHaveBeenCalledWith({
       name: 'valid_name',
-      email: 'valid_email',
+      email: 'valid_email@mail.com',
       password: 'hashed_value'
     })
   })
@@ -102,12 +102,31 @@ describe('DbAddAccount Use case', () => {
 
     const accountData = {
       name: 'valid_name',
-      email: 'valid_email',
+      email: 'valid_email@mail.com',
       password: 'valid_password'
     }
 
     const promise = sut.add(accountData)
 
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_value'
+    })
   })
 })
