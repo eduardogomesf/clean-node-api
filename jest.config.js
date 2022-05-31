b24f48615ba6f04e4f52b829055fa9373fe1aff9
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-05-30 23:58:37 -0300

    chore: remove jest config for in-memory mongodb

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
