47079fbd663cee3c562247d847f1a2aa42796f12
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-02 18:05:30 -0300

    test: ensure mongodb reconnects if client is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -12,7 +12,7 @@ describe('Signup routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
