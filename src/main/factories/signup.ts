ddfb39b09d2dd5d0f48e323b29dc4c86adb5f6e7
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-02 00:42:20 -0300

    refactor: fix folder name from criptography to cryptography

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,5 +1,5 @@
 import { DbAddAccount } from '../../data/use-cases/add-account/db-add-account'
-import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
