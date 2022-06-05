ed4f3e4994dbb0b2a51c6649e3477a4e6e58b658
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-05 17:15:08 -0300

    refactor: remove code duplication in LoginController

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -17,12 +17,12 @@ export class LoginController implements Controller {
     try {
       const { email, password } = httpRequest.body
 
-      if (!email) {
-        return badRequest(new MissingParamError('email'))
-      }
+      const requiredFields = ['email', 'password']
 
-      if (!password) {
-        return badRequest(new MissingParamError('password'))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
 
       const isEmailValid = this.emailValidator.isValid(email)
