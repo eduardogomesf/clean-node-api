d35dde8c8f090b96be9837e3b3a7eb10bac86f83
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-06 22:11:42 -0300

    refactor: group validations imports to a single file

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,8 +1,5 @@
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { CompareFieldsValidation, RequiredFieldValidation, ValidationComposite, EmailValidation } from '../../../presentation/helpers/validators'
 import { Validation } from '../../../presentation/protocols/validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { makeSignupValidation } from './signup-validation'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
 
