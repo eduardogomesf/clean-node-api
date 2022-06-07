9b374135a6f22b71282317b851eec5457fb92c87
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-06 22:04:56 -0300

    refactor: move Validation to protocols folder

--- src/presentation/controllers/login/login-protocols.ts
+++ src/presentation/controllers/login/login-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols'
 export * from '../../../domain/use-cases/authentication'
 export * from '../../helpers/validators/email-validation'
-export * from '../../helpers/validators/validation'
+export * from '../../protocols/validation'
