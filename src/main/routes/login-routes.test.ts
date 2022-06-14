46ffa8ebfff9885d29d711476d336edbfd8fbe9d
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-13 22:28:52 -0300

    test: ensure login route returns 401 on invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'edu@mail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
