222afab58890fde7db1dd083f2ca70607bc88830
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-21 12:48:52 -0300

    feat: add AddSurvey composition and route

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,36 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+
+describe('Login routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 204 on add survey', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
