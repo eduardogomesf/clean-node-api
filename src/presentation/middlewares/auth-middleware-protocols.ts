ef83c8418aa05ff3f5aaa8349137e332408d30ca
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-24 20:37:58 -0300

    refactor: move AuthMiddleware imports to a single file

---
+++ src/presentation/middlewares/auth-middleware-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../protocols'
+export * from '../../domain/use-cases/load-account-by-token'
+export * from '../../domain/models/account'
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,7 +1,6 @@
-import { LoadAccountByToken } from '../../domain/use-cases/load-account-by-token'
+import { LoadAccountByToken, HttpRequest, HttpResponse, Middleware } from './auth-middleware-protocols'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
-import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
   constructor (
