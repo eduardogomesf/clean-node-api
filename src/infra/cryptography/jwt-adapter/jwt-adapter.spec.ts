86264f670bc1c7eb0769e776a04b280e6d152785
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-25 15:31:54 -0300

    test: ensure JwtAdapter.decrypt throws if verify throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -52,5 +52,14 @@ describe('Jwt Adapter', () => {
       const value = await sut.decrypt('any_token')
       expect(value).toBe('any_value')
     })
+
+    it('Should throw if Jwt.verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementation(() => {
+        throw new Error()
+      })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
