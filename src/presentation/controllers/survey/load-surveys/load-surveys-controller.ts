1d4acfbd1a10379f170918b8d4a812f5d4ec0e00
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-26 17:39:30 -0300

    feat: ensure LoadSurveysController throws if LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,4 +1,4 @@
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../add-survey/add-survey-controller-protocols'
 import { Controller, LoadSurveys } from './load-surveys-controller-protocols'
 
@@ -8,7 +8,11 @@ export class LoadSurveysController implements Controller {
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
