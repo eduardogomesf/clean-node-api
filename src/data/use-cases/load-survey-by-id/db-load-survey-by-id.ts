b04016487e1f594ee9dd5f7c9aa3469cfaf21ca3
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-28 22:31:01 -0300

    feat: ensure DbLoadSurveyById  returns a survey on success

--- src/data/use-cases/load-survey-by-id/db-load-survey-by-id.ts
+++ src/data/use-cases/load-survey-by-id/db-load-survey-by-id.ts
@@ -8,7 +8,6 @@ export class DbLoadSurveyById implements LoadSurveyById {
   ) { }
 
   async loadById (id: string): Promise<SurveyModel> {
-    await this.loadSurveyByIdRepository.loadById(id)
-    return new Promise((resolve) => resolve(null))
+    return await this.loadSurveyByIdRepository.loadById(id)
   }
 }
