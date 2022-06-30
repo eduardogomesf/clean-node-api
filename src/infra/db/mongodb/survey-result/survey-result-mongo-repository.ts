e7fad00a4606c78a6b6b272c8c511226d3df3dd5
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-06-30 15:10:50 -0300

    feat: ensure SurveyResultMongoRepository insert new survey result if it is a new survey result

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,21 @@
+import { SaveSurveyResultModel, SaveSurveyResultRepository, SurveyResultModel } from '@/data/use-cases/save-survey-result/db-save-survey-result-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const res = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnDocument: 'after'
+    })
+    return res.value && MongoHelper.map(res.value)
+  }
+}
