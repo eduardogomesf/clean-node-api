b045869bc5de5bebe05c1712a4129698cc634528
Eduardo Gomes <eduardogoomesf@hotmail.com>
2022-07-02 13:49:54 -0300

    feat: add api to swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,35 @@
+import paths from './paths'
+import components from './components'
+import schemas from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
-    title: 'Clean Node API',
-    description: 'API para realizar enquete entre programadores',
-    version: '1.0.0'
-  }
+    title: 'Enquetes para Programadores',
+    description: 'API de enquete criada utilizando Node JS, TDD, DDD e Clean Architecture',
+    version: '1.0.0',
+    contact: {
+      name: 'Eduardo Gomes',
+      email: 'eduardo.gomes.fsd@gmail.com',
+      url: 'https://www.linkedin.com/in/eduardogomesf'
+    }
+  },
+  externalDocs: {
+    description: 'Link para o treinamento completo',
+    url: 'https://www.udemy.com/course/tdd-com-mango/?referralCode=B53CE5CA2B9AFA5A6FA1'
+  },
+  servers: [{
+    url: '/api',
+    description: 'Servidor Principal'
+  }],
+  tags: [{
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
+  }, {
+    name: 'Enquete',
+    description: 'APIs relacionadas a Enquete'
+  }],
+  paths,
+  schemas,
+  components
 }
